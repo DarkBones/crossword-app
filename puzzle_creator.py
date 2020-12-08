cf03dc778deb7750eaa45bb3dbbd3351cfe1a944
DarkBones <donkerbc@gmail.com>
2020-12-08 22:33:24 +0100

    Refactor

--- puzzle_creator.py
+++ puzzle_creator.py
@@ -2,63 +2,80 @@ import os
 import random
 import numpy as np
 import json
+import sys
+import getopt
 
 debug_mode = False
-
-dataset_path = os.path.join('data')
-words_path = os.path.join(dataset_path, 'words')
-clues_path = os.path.join(dataset_path, 'clues')
-puzzles_path = os.path.join(dataset_path, 'puzzles')
-
+max_words = 0
 min_height = 8
 
+def main(argv):
+	parse_args(argv)
 
-print('Getting words...')
-f = open(words_path)
-lines = f.readlines()
-
-words = {}
-n = 0
-max_n = 10000
-for line in lines:
-    if n >= max_n and max_n > 0:
-        break
-    
-    word = line.strip()
-    length = len(word)
-    
-    if not length in words:
-        words[length] = []
-    
-    words[length].append({ 'word': word, 'line': n})
-    
-    n += 1
-
-print(f'Found {len(words)} word lengths')
+	words = get_words()
 
 
-def create_puzzle(width, height, seed, illegal_down_words = 0, illegal_down_columns = 0):
-	if illegal_down_columns >= width:
-		if illegal_down_words >= len(words[height]):
-			return
-		return create_puzzle(width, height, seed, illegal_down_words + 1, 0)
+def get_words():
+	print_header('get words')
 
-	if illegal_down_words >= len(words[height]):
-		return
+	global max_words
 
-	np.random.seed(seed)
-	random.seed(seed)
+	words_path = os.path.join('data', 'words')
+	f = open(words_path)
+	lines = f.readlines()
 
-	down_index = np.random.permutation(len(words[height]))[illegal_down_words]
-	down = words[height][down_index]
-	print(down['word'])
+	words = {}
+	n = 0
+	for line in lines:
+		if n >= max_words and max_words > 0:
+			break
 
-# GENERATE PUZZLES
-seed_i = 0
-for height in sorted(words.keys()):
-	if height < min_height:
-		continue
+		word = line.strip()
+		length = len(word)
 
-	for width in sorted(words.keys()):
-		seed_i += 1
-		create_puzzle(width, height, seed_i)
+		if not length in words:
+			words[length] = []
+
+		words[length].append({ 'word': word, 'line': n})
+
+		n += 1
+
+	print(f'Found {n} words in {len(words)} different lengths')
+	return words
+
+
+def parse_args(argv):
+	global debug_mode, max_words, min_height
+
+	help_message = """
+usage: python puzzle_creator.py
+Options and arguments:
+-d\t: Debug mode, main 'puzzles' file not modified
+-h\t: print this help message and exit (also --help)
+-n arg\t: maximum number of words to use (also --max_words)
+-m arg\t: minimum puzzle height (also --min_height)
+	"""
+
+	try:
+		opts, args = getopt.getopt(argv, 'dhm:n:', ['help', 'max_words=', 'min_height'])
+	except getopt.GetoptError:
+		print(help_message)
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt in ['-h', '--help']:
+			print(help_message)
+			sys.exit()
+		elif opt == '-d':
+			debug_mode = True
+		elif opt in ['-n', '--max_words']:
+			max_words = int(arg)
+		elif opt in ['-m', '--min_height']:
+			min_height = int(arg)
+
+def print_header(header):
+	suff = '------------------------------------------------------'
+	print(f'\n-- {header.upper()} {suff[len(header):]}')
+
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
