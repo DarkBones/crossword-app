a00c1c69030a8dfb7a70be5eeb6da568da1f7117
DarkBones <donkerbc@gmail.com>
2020-12-11 14:38:53 +0100

    Create Word model

---
+++ db/migrate/20201211133521_create_word.rb
@@ -0,0 +1,13 @@
+class CreateWords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :words do |t|
+    	t.string :answer, null: false
+    	t.string :clue, null: false
+    	t.integer :length, null: false
+    	t.string :uuid, null: false
+      t.timestamps
+    end
+
+    add_index :card_items, :uuid
+  end
+end
---
+++ test/models/word_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+class WordTest < ActiveSupport::TestCase
+  # def test_model_creation
+  # end
+end
