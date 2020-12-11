1d70d502d817fda2f78b9744fc7d14607502a0f1
DarkBones <donkerbc@gmail.com>
2020-12-11 14:50:51 +0100

    Generator generator

---
+++ lib/generators/generator/templates/class.rb
@@ -0,0 +1,3 @@
+class <%%= class_name %> < Base<%%= class_name %>
+
+end
---
+++ lib/generators/generator/templates/generator.rb
@@ -0,0 +1,7 @@
+class <%= class_name %>Generator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  def create_files
+    template "#{file_name}.rb", "app/#{file_name.pluralize}/#{file_name}.rb"
+  end
+end
