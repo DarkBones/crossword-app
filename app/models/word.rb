a22bd003346d7e2cd5d3572da18e5b31d0a73a69
DarkBones <donkerbc@gmail.com>
2020-12-11 15:15:25 +0100

    Word creation

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,13 +1,17 @@
 class Word < ApplicationRecord
 	include Identifiable
-	
+
   # -- Constants ------------------------------------------------------------
 
   # -- Relationships --------------------------------------------------------
 
   # -- Callbacks ------------------------------------------------------------
+  before_validation :set_length, 		on: :create
+  before_validation :upcase_answer, on: :create
 
   # -- Validations ----------------------------------------------------------
+  validates :answer, 	presence: true
+  validates :clue, 		presence: true
 
   # -- Scopes ---------------------------------------------------------------
 
@@ -16,5 +20,11 @@ class Word < ApplicationRecord
   # -- Class Methods --------------------------------------------------------
 
   # -- Private Methods ------------------------------------------------------
+  private def set_length
+  	self.length = answer&.length || 0
+  end
 
+  private def upcase_answer
+  	self.answer = answer&.upcase
+  end
 end
--- test/models/word_test.rb
+++ test/models/word_test.rb
@@ -2,6 +2,35 @@ require 'test_helper'
 
 class WordTest < ActiveSupport::TestCase
   def test_uuid
-  	puts 'TEST MODEL CREATION'
+  	word = Word.create!(answer: 'test', clue: 'clue')
+
+  	assert_not_nil word.uuid
+  end
+
+  def test_length
+  	word = Word.create!(answer: 'test', clue: 'clue')
+
+  	assert_equal 4, word.length
+
+  	word = Word.create!(answer: 'longerword', clue: 'clue')
+
+  	assert_equal 10, word.length
+  end
+
+  def test_capitalization
+  	word = Word.create!(answer: 'LowerCase', clue: 'Clue')
+
+  	assert_equal 'LOWERCASE', word.answer
+  end
+
+  def test_validation
+  	word = Word.new(answer: 'test')
+  	assert_not word.valid?
+
+  	word = Word.new(clue: 'clue')
+  	assert_not word.valid?
+
+  	word = Word.new(answer: 'test', clue: 'clue')
+  	assert word.valid?
   end
 end
