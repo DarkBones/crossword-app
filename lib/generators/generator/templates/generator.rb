5273241ef3a2e8b61f61ecc8cdb09304822f2c44
DarkBones <donkerbc@gmail.com>
2020-12-11 14:53:13 +0100

    Bug fix

--- lib/generators/generator/templates/generator.rb
+++ lib/generators/generator/templates/generator.rb
@@ -2,6 +2,6 @@ class <%= class_name %>Generator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
   def create_files
-    template "#{file_name}.rb", "app/#{file_name.pluralize}/#{file_name}.rb"
+    template '<%= file_name %>.rb', "app/#{file_name.pluralize}/#{file_name}.rb"
   end
 end
